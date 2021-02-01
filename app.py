e8d9d8e635795f59549df466a12877b5a63b8c7b
EC2 Default User <ec2-user@ip-172-31-5-76.ec2.internal>
2021-02-01 20:26:51 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import os
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    random_number = random.randint(0,3)
+    artists = ['0OpWIlokQeE7BNQMhuu2Nx', '7dGJo4pcD2V6oG8kP0tJRR', '137W8MRPWKqSmrBGDBFSop'] #colt, eninem, wiz
+    return render_template('index.html')
+
+app.run(
+    port=int(os.getenv('PORT', 8080)),
+    host=os.getenv("IP", '0.0.0.0'),
+    debug=True
+)
