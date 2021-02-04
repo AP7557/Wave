2be8b28d8193d0f1aa56639f37f085b2748bcffa
AP7557 <ap.akash.patel1999+github@gmail.com>
2021-02-04 18:12:42 +0000

    Connect in flask

--- app.py
+++ app.py
@@ -12,18 +12,19 @@ def rand_song():
     data = resp(rand_artists())
     length = len(data['tracks'])
     random_number = random.randint(0,length-1)
-    return data['tracks'][random_number]['name']
+    return { 'name': data['tracks'][random_number]['name'],
+                'artists': data['tracks'][random_number]['album']['artists'][0]['name'],
+                'image': data['tracks'][random_number]['album']['images'][0]['url'],
+                'song_preview_url': data['tracks'][random_number]['external_urls']['spotify']
+            }
 
-print(rand_song())
-# app = Flask(__name__)
-# # load_dotenv(find_dotenv())
-# @app.route('/')
-# def hello_world():
-#
-#     return render_template('index.html')
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return render_template('index.html', rand_song())
 
-# app.run(
-#     port=int(os.getenv('PORT', 8080)),
-#     host=os.getenv("IP", '0.0.0.0'),
-#     debug=True
-# )
+app.run(
+    port=int(os.getenv('PORT', 8080)),
+    host=os.getenv("IP", '0.0.0.0'),
+    debug=True
+)
